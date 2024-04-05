d0db88d603b502a97170df9247796d470572de3a
Parth Thakar <parththakar59@gmail.com>
2024-04-05 19:12:18 +0530

    Added primary and secondary App theme color.

--- app/src/main/java/com/scandog/ui/theme/Color.kt
+++ app/src/main/java/com/scandog/ui/theme/Color.kt
@@ -9,3 +9,7 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val appMainColor = Color(0xFF2F4FCD)
+val appBackgroundLight = Color(0xFFFFFFFF)
+val appBackgroundDark = Color(0xFF070707)
